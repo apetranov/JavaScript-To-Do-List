1aefb65b278134f5dfd4e81c27b726752b6cf656
Alexander Petranov <shasho68.5@gmail.com>
2026-01-07 18:51:17 +0200

    Add show active feature

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let addTask = document.querySelector('#addTask')
 let tasksDiv = document.querySelector('#tasksDiv')
 let showAllBtn = document.getElementById('showAll')
 let showCompletedBtn = document.getElementById('showCompleted')
+let showActiveBtn = document.getElementById('showActive')
 
 
 let tasks = []
@@ -52,6 +53,47 @@ function displayCompleted() {
      tasksDiv.innerHTML = '';
     for (let i = 0; i < tasks.length; i++) {
      if (tasks[i].completed)    {
+        let task = document.createElement('div')
+        let taskText = document.createElement('h1')
+        taskText.textContent = tasks[i].text
+        task.appendChild(taskText)
+
+        let buttonsDiv = document.createElement('div')
+        let complete = document.createElement('span')
+        complete.textContent = '✅'
+        complete.classList.add('cursor-pointer')
+        complete.addEventListener('click', () => {
+            taskText.classList.toggle('completed')
+        })
+
+        let del = document.createElement('span')
+        del.textContent = '🗑️'
+        del.classList.add('cursor-pointer')
+        del.addEventListener('click', () => {
+            tasksDiv.removeChild(task)
+        })
+
+        buttonsDiv.appendChild(complete)
+        buttonsDiv.appendChild(del)
+        task.appendChild(buttonsDiv)
+
+        task.classList.add('shadow-xl')
+        task.classList.add('rounded-2xl')
+        task.classList.add('px-7')
+        task.classList.add('py-3')
+        task.classList.add('flex')
+        task.classList.add('justify-between')
+        task.classList.add('space-x-10')
+        tasksDiv.appendChild(task)
+     }
+    
+    }
+}
+
+function displayActive() {
+     tasksDiv.innerHTML = '';
+    for (let i = 0; i < tasks.length; i++) {
+     if (!tasks[i].completed)    {
         let task = document.createElement('div')
         let taskText = document.createElement('h1')
         taskText.textContent = tasks[i].text
@@ -146,4 +188,8 @@ showAllBtn.addEventListener('click', () => {
 
 showCompletedBtn.addEventListener('click', () => {
     displayCompleted();
+})
+
+showActiveBtn.addEventListener('click', () => {
+    displayActive();
 })
